059bb453a79d0fb2afc8e58caae6a47a7d472d5b
Fle2x <dotsfam87@Mail.ru>
2020-10-06 23:10:59 +0500

    V1_1

---
+++ ijunior-lesson-9/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+[RequireComponent(typeof(Player))]
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _healthSlider;
+    [SerializeField] private Player _player;
+
+    private void Start()
+    {
+        _healthSlider.minValue = _player.minHealth;
+        _healthSlider.maxValue = _player.maxHealth;
+    }
+
+    public void SetHealthSlider(float health)
+    {
+        _healthSlider.value = health;
+    }
+}
---
+++ ijunior-lesson-9/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Text _healthValue;
+    [SerializeField] private HealthBar _healthBar;
+
+    [SerializeField] private float _currentHealth = 100f;
+    [SerializeField] private float _maxHealth = 100f;
+    [SerializeField] private float _minHealth = 0f;
+
+    public float maxHealth => _maxHealth;
+    public float minHealth => _minHealth;
+
+    void Update()
+    {
+        _healthValue.text = _currentHealth.ToString("0");
+       
+        if (_currentHealth >= _maxHealth)
+        {
+            _currentHealth = _maxHealth;
+            _healthBar.SetHealthSlider(_currentHealth);
+        }
+
+        if (_currentHealth <= _minHealth)
+        {
+            _currentHealth = _minHealth;
+            _healthBar.SetHealthSlider(_currentHealth);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        _currentHealth -= damage;
+        _healthBar.SetHealthSlider(_currentHealth);
+    }
+
+    public void SetHealth(float health)
+    {
+        _currentHealth += health;
+        _healthBar.SetHealthSlider(_currentHealth);
+    }
+}
